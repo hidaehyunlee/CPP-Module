af750d0da1a7a7073085f1c248818a323456beba
Daehyun Lee <leehi0210@gmail.com>
2021-04-28 17:30:42 +0900

    Add more tests

--- CPP_Module04/ex00/main.cpp
+++ CPP_Module04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: daelee <daelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 15:05:11 by daelee            #+#    #+#             */
-/*   Updated: 2021/04/28 17:00:19 by daelee           ###   ########.fr       */
+/*   Updated: 2021/04/28 17:27:42 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,29 @@
 
 int main(void)
 {
-    Sorcerer robert("Robert", "the Magnificent");
-    Victim jim("Jimmy");
-    Peon joe("Joe");
-    std::cout << robert << jim << joe;
-    robert.polymorph(jim);
-    robert.polymorph(joe);
+    {
+        Sorcerer robert("Robert", "the Magnificent");
+        
+        Victim jim("Jimmy");
+        Peon joe("Joe");
+        
+        std::cout << robert << jim << joe;
+        
+        robert.polymorph(jim);
+        robert.polymorph(joe);
+    }
+    std::cout << std::endl;
+    std::cout << "--------------- some more tests ---------------" << std::endl;
+    std::cout << std::endl;
+    {
+        Sorcerer robert("Robert", "the Magnificent");
+        Victim jim("Jimmy");
+        Peon joe("Joe");
 
+        Victim *newjoe = &joe;
+
+        robert.polymorph(joe);
+        robert.polymorph(*newjoe);
+    }
     return 0;
 }
